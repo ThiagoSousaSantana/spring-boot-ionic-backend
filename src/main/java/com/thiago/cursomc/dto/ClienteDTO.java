bc1eb247cb3f51032a08c85aa8ce536555626a74
Thiago Sousa Santana <thiago91098@gmail.com>
2019-01-31 22:18:28 -0200

    PUT, DELETE e POST para Cliente

---
+++ src/main/java/com/thiago/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,55 @@
+package com.thiago.cursomc.dto;
+
+import com.thiago.cursomc.domain.Cliente;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+public class ClienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser de 5 a 120 caracteres")
+    private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Email(message = "Email invalido")
+    private String email;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Cliente obj) {
+        this.id = obj.getId();
+        this.email = obj.getEmail();
+        this.nome = obj.getNome();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/thiago/cursomc/services/ClienteService.java
+++ src/main/java/com/thiago/cursomc/services/ClienteService.java
@@ -1,11 +1,19 @@
 package com.thiago.cursomc.services;
 
 import com.thiago.cursomc.domain.Cliente;
+import com.thiago.cursomc.domain.Cliente;
+import com.thiago.cursomc.dto.ClienteDTO;
 import com.thiago.cursomc.repositories.ClienteRepository;
+import com.thiago.cursomc.services.exceptions.DataIntegrityException;
 import com.thiago.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +26,38 @@ public class ClienteService {
 		Optional<Cliente> cliente = repository.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id:" + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente update(Cliente obj){
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		try {
+			repository.delete(find(id));
+		}catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possivel excluir Cliente que contem pedidos!");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO dto){
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
